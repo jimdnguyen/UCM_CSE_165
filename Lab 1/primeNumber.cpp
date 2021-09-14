22eca50aef51c11e4ea8a73b91e48bad084cb7cd
DaoistDuck <dragonoath123@gmail.com>
2021-09-13 20:38:24 -0700

    got primenumber to work
    finished primenumber and got it to work

--- Lab 1/primeNumber.cpp
+++ Lab 1/primeNumber.cpp
@@ -4,34 +4,44 @@
 
 int main()
 {
-    int range = 10;
-    int counter = 1;
+    int range = 1000;
+    int counter = 0; 
     std::vector<int> primeNumbers;
-    primeNumbers.push_back(1);
-    //primeNumbers.push_back(2);
+    primeNumbers.push_back(2); //initialize the vector with the first prime number: 2 to calculate the rest of the prime numbers
 
-    for(int i = 1; i < range; i++)
+    for(int i = 2; i < range; i++) // this for loop goes through the whole range to find all the prime numbers it can
     {   
-       if(i % 2 != 0)
+       if(i % 2 != 0) // first, we get rid of all the even numbers since they can't be odd
        {
-           for(int j = i; j < primeNumbers.size(); j++)
+           for(int j = 0; j < primeNumbers.size(); j++) // then we iterate through our current list of prime numbers and see if the current number is divisible by any of them
            {
-               if(j % 2 != 0)
-               {
-                   counter = counter + 1;
-               }
-               if(counter == primeNumbers.size()){
-                   primeNumbers.push_back(j);
-                   counter = 0;
-               }
+               
+                if(i % primeNumbers[j] == 0) // if the current number is divisible by any primenumber inside the vector, we skip it since it isn't prime
+                {
+                   break;
+                }
+                else
+                {
+                    counter ++;
+                }
+            
+                if(counter == primeNumbers.size()) // since the current number isn't divisible by any of them, we add it to the primenumber vector.
+                {
+                   primeNumbers.push_back(i);
+                }
+                
            }
+           counter = 0;
+            
        }
+       
     }
 
-    for(int z = 0; z < primeNumbers.size(); z++)
+    for(int z = 0; z < primeNumbers.size(); z++) // then we just print out all the prime numbers we found
     {
         std::cout<<primeNumbers[z] << std::endl;
     }
+    
 
     return 0;
 }
