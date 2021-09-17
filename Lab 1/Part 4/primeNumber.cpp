8a5c450ab14edc72a0a53c99e8a7990b878202f8
DaoistDuck <dragonoath123@gmail.com>
2021-09-17 02:06:02 -0700

    hi
    hi

--- Lab 1/Part 4/primeNumber.cpp
+++ Lab 1/Part 4/primeNumber.cpp
@@ -11,14 +11,14 @@ int main()
 
     for(int i = 2; i < range; i++) // this for loop goes through the whole range to find all the prime numbers it can
     {   
-       if(i % 2 != 0) // first, we get rid of all the even numbers since they can't be odd
-       {
-           for(int j = 0; j < primeNumbers.size(); j++) // then we iterate through our current list of prime numbers and see if the current number is divisible by any of them
-           {
-               
+       
+        for(int j = 0; j < primeNumbers.size(); j++) // then we iterate through our current list of prime numbers and see if the current number is divisible by any of them
+        {
+            if(i % 2 != 0) // first, we get rid of all the even numbers since they can't be odd
+            {    
                 if(i % primeNumbers[j] == 0) // if the current number is divisible by any primenumber inside the vector, we skip it since it isn't prime
                 {
-                   break;
+                    break;
                 }
                 else
                 {
@@ -27,21 +27,20 @@ int main()
             
                 if(counter == primeNumbers.size()) // since the current number isn't divisible by any of them, we add it to the primenumber vector.
                 {
-                   primeNumbers.push_back(i);
+                    primeNumbers.push_back(i);
                 }
+            }
                 
-           }
-           counter = 0;
-            
-       }
-       
+        }
+        counter = 0;
     }
 
     for(int z = 0; z < primeNumbers.size(); z++) // then we just print out all the prime numbers we found
     {
         std::cout<<primeNumbers[z] << std::endl;
     }
-    
 
     return 0;
 }
+
+// I used this website to see if my answers matched up with this image https://www.geekgirlauthority.com/monday-math-mathematician-kp-hart-prime-number-systems/
