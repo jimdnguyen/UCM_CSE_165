153a3d759015145e542dee8c2ea8ff4c674a3cec
DaoistDuck <dragonoath123@gmail.com>
2021-11-06 15:57:43 -0700

    added comments

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -45,11 +45,15 @@ int main()
 }
 
 /*
-Links used to help me learn and understand OpenGL and Soil
+Links used to help me learn and understand OpenGL
 https://medium.com/@vivekjha92/setup-opengl-with-vs-code-82852c653c43
 https://www.glfw.org/download.html
 https://learnopengl.com/Getting-started/Hello-Window
 https://code.visualstudio.com/docs/cpp/config-mingw#_build-helloworldcpp
 https://glad.dav1d.de/
 //compile in cmd not wsl, https://stackoverflow.com/questions/66127102/command-cmd-not-found-but-there-are-16-similar-ones
+*/
+
+/*
+Links used to help me learn and understand SOIL
 */
